b8e983bd252f410867254fee59cfbc8b55ae6a5a
harikesh <harikeshkolekar@gmail.com>
2016-06-06 12:24:56 +0530

    successfully finding sudo weather of entered city

--- app/app.component.ts
+++ app/app.component.ts
@@ -62,6 +62,30 @@ export class AppComponent {
         this.weatherOfCities = [];
 	}
 
+    getWeather = function(city: string) {
+        var weather: Weather;
+        if (city.toLocaleLowerCase() == "vienna") { //IF ENTERED CITY IS VIENNA RETURN BELOW WEATHER OBJECT
+            weather = {
+                "id": 1,
+                "city": "vienna",
+                "main": "Clouds",
+                "description": "overcast clouds"
+            };
+        } else if (city.toLocaleLowerCase() == "london") {
+            // RETURN BELOW OBJECT IF SEARCHED CITY IS LONDON
+            weather = {
+                "id": 2,
+                "city": "london",
+                "main": "Rain",
+                "description": "very heavy rain"
+            };
+        } else {
+            // IF SEARCHED CITY DOESN'T EXIST
+            
+        }
+        return weather;
+    }
+
     addCity(city: string, $event: any) {    
     //addCity = function(city: string, $event:any) {
         if ($event.keyCode == 13) {
